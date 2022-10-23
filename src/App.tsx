5da992f0e3f72d11a3118a60f10ffa99b7156028
ryskit <ryosukekida.71@gmail.com>
2022-10-24 02:22:29 +0900

    :sparkles: カスタムフックを使うようにコンポーネントを書き換えた

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { userProfile } from "./types/userProfile";
+
+import { useAllUsers } from "./hooks/useAllUsers";
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
@@ -42,7 +18,7 @@ function App() {
       ) : (
         <React.Fragment>
           {userProfiles.map((user) => (
-            <UserCard user={user} />
+            <UserCard key={user.id} user={user} />
           ))}
         </React.Fragment>
       )}
